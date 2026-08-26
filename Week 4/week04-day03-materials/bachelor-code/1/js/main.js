ae4c2aa20fd28c10b61361154d76b01e3f325e07
Kinza Rehman (Ext) <kinza.rehman@optimum.com>
2026-08-26 14:27:07 -0400

    Update project

--- Week 4/week04-day03-materials/bachelor-code/1/js/main.js
+++ Week 4/week04-day03-materials/bachelor-code/1/js/main.js
@@ -1,6 +1,17 @@
-document.querySelector('#finalRose').addEventListener('click', /*INSERTCODE*/)
+document.querySelector('#finalRose').addEventListener('click', hide)
 
 function hide(){
-	document.querySelector(/*INSERTCODE*/).style.display = 'none'
-	document.querySelector(/*INSERTCODE*/).style.display = 'none'
+	document.querySelector('#claire').style.display = 'none'
+	document.querySelector('#sharleen').style.display = 'none'
+}
+
+document.querySelector('#finalRose').addEventListener('click', show)
+
+
+//another solution but more work? 
+function show(){
+	document.querySelector('#claire').style.display = 'none'
+	document.querySelector('#sharleen').style.display = 'none'
+	document.querySelector('#nikki').style.display = 'block'  
+	// you have to explicietly remove the other two 
 }
