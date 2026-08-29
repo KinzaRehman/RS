425d3d0d1b70b525fd07fa9d3aaed29633bc8fcd
Kinza Rehman <kinzar689@gmail.com>
2026-08-29 19:00:44 -0400

    new answers

--- Week 4/week04-day04-materials/week04-day04-materials/age-checker/js/main.js
+++ Week 4/week04-day04-materials/week04-day04-materials/age-checker/js/main.js
@@ -39,35 +39,58 @@
 //Take the value from the input
 //Place the result of the conditional in the paragraph
 
-document.querySelector('h1').addEventListener('click', checkAge)
+//Create a conditonal that checks their age
+
+//On click of the h1
+
+document.querySelector('h1').addEventListener('click', checkAge);
 
 function checkAge() {
-    const age = Number(document.querySelector('input').value);
-    console.log(age)
-      if (age <16) {
-        console.log( "You're too young to drive")
-    }
 
-    else if (age < 18) {
-        console.log( "You can't hate from outside the club, because they can't even get in")
-    }
-    else if (age < 21) {
-        console.log( "You can not drink")
-    }
-    else if (age < 25) {
-        console.log( "cant rent cars affordably")
-    }
+  //Take the value from the input
 
-    else if (age < 30) {
-        console.log( "You cant rent fancy cars affordably")
-    }
-    else if (age > 30) {
-        console.log( "theres nothing left to look forward to ")
-    }
-    else {
-        console.log('more than 30 huh')
-    }
- 
+  const age = Number(document.querySelector('#danceDanceRevolution').value);
+
+  let message;
+
+  //If under 16, tell them they can not drive
+
+  if (age < 16) {
+    message = "You're too young to drive";
+  }
+
+  //If under 18, tell them they can't hate from outside the club, because they can't even get in
+
+  else if (age < 18) {
+    message =
+      "You can't hate from outside the club, because you can't even get in";
+  }
+
+  //If under 21, tell them they can not drink
+
+  else if (age < 21) {
+    message = "You cannot drink";
+  }
+
+  //If under 25, tell them they can not rent cars affordably
+
+  else if (age < 25) {
+    message = "You can't rent cars affordably";
+  }
+
+  //If under 30, tell them they can not rent fancy cars affordably
+
+  else if (age < 30) {
+    message = "You can't rent fancy cars affordably";
+  }
+
+  //If 30 or over, tell them there is nothing left to look forward to
+
+  else {
+    message = "There's nothing left to look forward to";
+  }
+
+  //Place the result of the conditional in the paragraph
+
+  document.querySelector('p').innerText = message;
 }
-
-
--- Week 4/week04-day04-materials/week04-day04-materials/bring-it-on/js/main.js
+++ Week 4/week04-day04-materials/week04-day04-materials/bring-it-on/js/main.js
@@ -1,71 +1,85 @@
 // *Variables*
+
 // Create a variable and console log the value
-let color = "pink"
-console.log(color); 
+
+let color = "pink";
+
+console.log(color);
+
 
 // Create a variable, add 10 to it, and alert the value
-let var2 = 30
+
+let var2 = 30;
+
 let var3 = var2 + 10;
-alert(var3); 
+
+alert(var3);
+
 
 // *Functions*
+
 // Create a function that subtracts 4 numbers and alerts the difference
-function subtract (n1,n2,n3,n4){
-    let difference = n1 - n1 - n3 - n4
-    alert(difference)
+
+function subtract(n1, n2, n3, n4) {
+  let difference = n1 - n2 - n3 - n4;
+
+  alert(difference);
 }
 
 subtract(10, 2, 3, 4);
 
+
 // Create a function that divides one number by another and returns the remainder
 
 function division(num1, num2) {
-    let div = (num1 % num2)
-    return(div)
+  let remainder = num1 % num2;
+
+  return remainder;
 }
-console.log(division(10,3)); 
+
+console.log(division(10, 3));
 
 
 // *Conditionals*
-// Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
-function twonums(num1, num2) { 
-    num1 += num2; 
 
-    if (num2 > 50) {
-        alert('JUMANJI')
-    } 
+// Create a function that adds two numbers and if the sum is greater than 50 alert JUMANJI
 
+function twoNums(num1, num2) {
+  let sum = num1 + num2;
+
+  if (sum > 50) {
+    alert("JUMANJI");
+  }
 }
-console.log(twonums(24, 1));
-console.log(twonums(30,40));
+
+twoNums(24, 1);
+
+twoNums(30, 40);
 
 
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
-function Multi(num1, num2, num3) { 
-    let multiply = num1 * num2 * num3
-    if (multiply % 3 ===0) {
-        alert('ZEBRA')
-    }
-    else {
-        alert('not zerba')
-    }
+
+function multiply(num1, num2, num3) {
+  let product = num1 * num2 * num3;
+
+  if (product % 3 === 0) {
+    alert("ZEBRA");
+  }
 }
 
-console.log(Multi(9,9,9)); 
+multiply(9, 9, 9);
 
 
 //*Loops*
 //Create a function that takes in a word and a number. Console log the word x times where x was the number passed in
-function swag(word, num) {
-    for (let count = 0;
-        count < num; 
-        count++) {
-            console.log(word)
-        }
-// return(word('picnic', 4))
-}
-console.log(swag('pink',3));
 
+function swag(word, num) {
+  for (let count = 0; count < num; count++) {
+    console.log(word);
+  }
+}
+
+swag("pink", 3);
 // recursion 
 // for loops 
 // return
