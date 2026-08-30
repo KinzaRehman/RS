e4a0d5c64dd4a18fef338c196f24c061dc3f5dcd
Kinza Rehman <kinzar689@gmail.com>
2026-08-29 23:35:34 -0400

    flower

--- Week 4/Week4-assignments/Calculator/js/main.js
+++ Week 4/Week4-assignments/Calculator/js/main.js
@@ -1,1 +1,50 @@
-// alert('works')
+//alert('works')
+
+// Addition takes id add to add the two numbers
+//subtractions takes id sub to subtract num 1 from num 2,
+//multiplication takes id mul ti multiple two values 
+//division takes id div to divide two values using operands? %
+
+document.querySelector('#add').addEventListener('click', addTwoNums);
+document.querySelector('#sub').addEventListener('click', subTwonums);
+document.querySelector('#mul').addEventListener('click', mulTwoNums );
+document.querySelector('#div').addEventListener('click', divTwoNums);
+
+
+
+
+//addution of two numbers
+function addTwoNums(){
+    let num1 = Number(document.querySelector('#num1').value);
+    let num2 =Number(document.querySelector('#num2').value);
+
+    let sum = num1 + num2 ;
+    document.querySelector('#theAnswer').innerText = sum;
+}
+
+//subtraction two numbers
+function subTwonums(){
+    let num1 = Number(document.querySelector('#num1').value);
+    let num2 =Number(document.querySelector('#num2').value);
+
+    let diff = num1 - num2 ;
+    document.querySelector('#theAnswer').innerText = diff;
+}
+
+//Multiplication fo two numbers 
+function mulTwoNums() {
+    let num1 = Number(document.querySelector('#num1').value);
+    let num2 = Number(document.querySelector('#num2').value)
+
+    let multi = num1 * num2; 
+    document.querySelector('#theAnswer').innerText = multi; 
+}
+
+//dividing two numbers 
+function divTwoNums() {
+    let num1 = Number(document.querySelector('#num1').value);
+    let num2 = Number(document.querySelector('#num2').value);
+
+    let div = num1 / num2 ;
+    document.querySelector('#theAnswer').innerText = div; 
+}
--- Week 4/Week4-assignments/Date & guess/js/main.js
+++ Week 4/Week4-assignments/Date & guess/js/main.js
@@ -1,2 +1,145 @@
 // alert('works')
+
+{//logic for seasons
+//Winter (decemebr 21st-March 19th)
+//Spring (March 20th- June 20th)
+//Summer (June 21st- September 22nd)
+//Autumn (sep 23rd- december 20)
+}    
+ 
 // seasonal flowers, for each month which flowers are in season around the year in the unties states? 
+{//these are all the months but in html 
+{
+//January 
+// January is Winter Elegance!
+// some in season flwoers: 
+// -Amaryllis
+// -Anemones
+// -Paperwhites
+// -Ranunchulus
+// -Hellebores
+// -Orchids
+}
+{
+//February
+// February is red hearts!
+// some in season flowers:
+// -Roses
+// -Tulips
+// -Sweet Peas
+// -Camellias
+// -Ranunchulus
+// -Hyacinths 
+}
+{
+// March
+// March is spring awakening!
+// Some in season flowers
+// -Daffodils
+// -Tulips
+// -Irises
+// -Hyacinths
+// -Freesia
+// -Pussy willows
+}
+{
+// April
+// April is peak Spring!
+// Some in season flowers:
+// -Peonies
+// -Tulips
+// -Cherry blossoms 
+// -Lily of the valley
+// -Ranunchulus
+}
+{
+//May
+// May is peony Season!
+// Some in season flowers:
+// -Peonies
+// -Garden roses
+// -Lilacs
+// -Snapdargons
+// -Allium
+}
+{
+//June
+//June is the officially summer!
+// some in season flowers:
+// -Peonies
+// -Roses
+// -Hydranges
+// -Delphinium
+//-Lsianthus
+// -Lavender
+}
+{
+//July
+//July is high summer
+// Some in season flowers:
+// -Sunflowers
+// -Dahlias
+// -Zinnias 
+// -Cosmos
+// -Gladiolus
+// -Roses
+}
+{
+//August
+//August is late summer abundance!!
+// Some in season flowers:
+// -Dahlias
+// -Sunflowers 
+// -Tuberose 
+// -Lisiantus
+// -Aesters
+// -Celosia
+}
+{
+//September
+//September is tansitioning to fall! 
+// Some in season flowers:
+// -Dahlias
+// -Chryanthemums
+// -Marigolds
+// -Sedum
+// -Roses
+// -Ornamental grass
+}
+
+{
+//October 
+//October is peak fall!
+// Some in season flowers:
+// - Chrysanthemums 
+// - Dahlias
+// -Calla Lilies
+// - Hypericum berries
+// -Bittersweet
+// -Ornamental Kale
+}
+{
+//November
+//Novemebr is late Autumn!
+// Some in season flowers:
+// - Chrysanthemums 
+// - roses
+// -Carnations 
+// -Protea 
+// -Eucalyptus
+// -Dired Flowers 
+}
+{
+//December 
+//December means you've made it!
+// Some in season flowers:
+// -Poinsettias
+// -Amaryllis
+// -Paperwhites
+// -Red roses
+// -Holly and evergreen 
+// -Orchids 
+}
+}
+
+
