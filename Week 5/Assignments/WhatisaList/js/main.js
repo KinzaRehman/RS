7413532babbb50a2a213334333997a22400a387a
Kinza Rehman <kinzar689@gmail.com>
2026-09-03 14:26:29 -0400

    its new things

--- Week 5/Assignments/WhatisaList/js/main.js
+++ Week 5/Assignments/WhatisaList/js/main.js
@@ -1,1 +1,19 @@
-// alert('works')
+//alert('works')
+
+
+
+console.log('works');
+
+
+function addTask() {
+    const taskInput = document.getElementById('task-input');
+    if (!taskInput.value) {
+        alert('add some texts of things');
+        return;
+    }
+
+    const li = document.createElement('li');
+
+    li.innerHTML = <label><input type="checkbox" class="task-checkbox"> ${taskInput.value} <button class="delete-button">Delete</button></label>;
+
+}
