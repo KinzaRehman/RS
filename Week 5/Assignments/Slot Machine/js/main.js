a38802ecd92c35819f9555a8804aa3e7408ba165
Kinza Rehman <kinzar689@gmail.com>
2026-09-03 10:20:37 -0400

    renamed these things

--- Week 5/Assignments/Slot Machine/js/main.js
+++ Week 5/Assignments/Slot Machine/js/main.js
@@ -1,12 +1,12 @@
 // alert('works')
 const minBet = 5;
 const maxBet = 15; 
-let balance = 100;
+let balance
 
 function slotmachine(bet) {
     //check our inut 
-    if bet <= 0 {
-        throw new Error("Bet must be greater than 0");
+    if (bet <= 0) {
+        throw new Error("Bet must be greater than 0")
     }
 
 }
