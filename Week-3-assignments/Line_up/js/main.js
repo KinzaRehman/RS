5422e1a93817047b45cee03cb5fd96381d0c4719
Kinza Rehman <kinzarehman38@gmail.com>
2026-09-09 14:39:14 -0400

    updating another one

--- Week-3-assignments/Line_up/js/main.js
+++ Week-3-assignments/Line_up/js/main.js
@@ -1,1 +1,1 @@
-// alert('works')
+//alert("working")
--- Week-3-assignments/level-ground/js/main.js
+++ Week-3-assignments/level-ground/js/main.js
@@ -1,1 +1,10 @@
 // alert('works')
+const donateButton = document.querySelector("header > a");
+
+donateButton.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  alert(
+    "This is a sample website. Please reach out to kinzarehman38@gmail.com to explore website options for you!"
+  );
+});
